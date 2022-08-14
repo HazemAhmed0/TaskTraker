85cc8ef9a7f5afa1201ad629a4886ec0ceccfdd7
Hazem <85453727+HazemAhmed0@users.noreply.github.com>
2022-08-14 14:37:33 +0200

    added: slide to delete tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,24 +1,31 @@
 import "../styles/main.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useSwipeable } from "react-swipeable";
 
-const task = ({ task, onDelete, onDone, onProgress }) => {
+const Task = ({ task, onDelete, onDone, onProgress }) => {
   var classes = task.status + " task";
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      onDelete(task.id);
+    },
+  });
   return (
-    <div className={classes}>
-      <h3>{task.text}</h3>
-      <p>{task.desc}</p>
-      <a title="In Progress" onClick={() => onProgress(task.id)}>
-        &#9994;
-      </a>
-      <a title="Edit Task">&#128221;</a>
-      <a title="Remove Task" onClick={() => onDelete(task.id)}>
-        &#10062;
-      </a>
-      <a title="Complete Task" onClick={() => onDone(task.id)}>
-        &#9989;
-      </a>
+    <div {...handlers}>
+      <div className={classes}>
+        <h3>{task.text}</h3>
+        <p>{task.desc}</p>
+        <a title="In Progress" onClick={() => onProgress(task.id)}>
+          &#9994;
+        </a>
+        <a title="Edit Task">&#128221;</a>
+        <a title="Remove Task" onClick={() => onDelete(task.id)}>
+          &#10062;
+        </a>
+        <a title="Complete Task" onClick={() => onDone(task.id)}>
+          &#9989;
+        </a>
+      </div>
     </div>
   );
 };
 
-export default task;
+export default Task;
